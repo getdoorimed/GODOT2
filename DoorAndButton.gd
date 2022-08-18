3dd893471c2cf76b864411962fcc6a0e350f3e86
George Montalban <geomon00@kingshigh.school.nz>
2022-08-19 10:58:24 +1200

    Save3

--- DoorAndButton.gd
+++ DoorAndButton.gd
@@ -1,16 +1,11 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AnimationPlayer.play("ButtonUp")
+	$AnimationPlayer.play("DoorClosed")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("Player"):
+		$AnimationPlayer.play("ButtonDown")
+		$AnimationPlayer.play("DoorOpen")
+		
