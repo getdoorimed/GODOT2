b57a71bc9fa980154fa45b406b974ea7feb47ac0
getdoorimed <100237138+getdoorimed@users.noreply.github.com>
2022-10-28 02:06:18 +1300

    title screen

--- Scripts/sound_player.gd
+++ Scripts/sound_player.gd
@@ -3,12 +3,12 @@ extends Node
 onready var music = AudioStreamPlayer.new()
 
 var music_tracks = {
-	"title_track":"res://sfx/Music/[YT2mp3.info] - Alan Walker - Fade [COPYRIGHTED NCS Release] (320kbps).mp3",
-	"main":"res://sfx/Music/[YT2mp3.info] - alan walker - faded (ncs release) at very low quality (320kbps).mp3",
+	"main":"res://sfx/Music/[YT2mp3.info] - Alan Walker - Fade [COPYRIGHTED NCS Release] (320kbps).mp3",
+	"title_track":"res://sfx/Music/[YT2mp3.info] - alan walker - faded (ncs release) at very low quality (320kbps).mp3",
 }
 
 var sound_effects = {
-	
+	"jump":"res://sfx/Music/[YT2mp3.info] - Retro 8 bit Jump Sound Effect for FREE!! iggchan (320kbps).mp3",
 	
 	
 	
@@ -24,6 +24,6 @@ func change_sound_db(val):
 	sound_db = linear2db(val)
 	
 func _ready():
-	music.stream = load(music_tracks("main"))
+	music.stream = load(music_tracks["main"])
 	add_child(music)
 	music.play()
